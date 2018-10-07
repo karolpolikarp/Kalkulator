948b048e59153748cf1ba2982fda0bfe33c8370a
karolpolikarp <karolpwilczynski@gmail.com>
2018-10-07 02:18:25 +0200

    zmiany2

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -3,6 +3,12 @@ package com.kodilla.kalkulator;
 public class Calculator {
     public void add(){
     }
-    public void reduce() {
+    public void reduce()
+    {
+        System.out.println("It works!");
+    }
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.reduce();
     }
 }
